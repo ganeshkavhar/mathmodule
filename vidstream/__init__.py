775f4726b030fb9110fd9b2618097c78389e7a16
ganesh kavhar <kavharg@gmail.com>
2023-10-22 07:12:42 +0530

    Add files via upload

---
+++ vidstream/__init__.py
@@ -0,0 +1,3 @@
+from vidstream.streaming import *
+print(sum(20,30))
+print(mul(3,5))
---
+++ vidstream/streaming.py
@@ -0,0 +1,13 @@
+
+
+__author__ = "ganesh kavhar"
+__email__ = "ganeshkavhar8@gmail.com"
+__status__ = "planning"
+
+def add(a,b):
+    c=a+b
+    return c
+
+def mul(a,b):
+    c=a*b
+    return c
